25a5792c53e3a2a5b1900e521f0e8bd19ef3da7e
Rafael S. Gomes <silveiragomesr@gmail.com>
2024-05-20 14:28:12 -0300

    feat: implementa rota de cadastro de usuarios

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import express from 'express'
+import { userRouter } from './routes/userRouter.js'
 
 const app = express()
+app.use(express.json())
+
+app.use('/user', userRouter)
 
 app.listen(3000, () => console.log('app rodando...'))
