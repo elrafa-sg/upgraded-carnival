c4353d7f711a9cbd9b19ee41be614e69d2a58a44
Rafael S. Gomes <silveiragomesr@gmail.com>
2024-05-21 12:28:04 -0300

    feat: adiciona documentação da api com swagger

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import express from 'express'
+import swaggerUi from 'swagger-ui-express'
 
 import './dotenv.config'
 import router from './routes/index'
+
+import swaggerDocs from '../swagger.json'
 
 const app = express()
 app.use(express.json())
 
 app.use(router)
+
+// swagger docs
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 
 app.listen(3000, () => console.log('app rodando...'))
