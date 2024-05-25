cab69a2d016fd803efbcd7526464e16d748cb436
Rafael S. Gomes <silveiragomesr@gmail.com>
2024-05-25 17:58:50 -0300

    fix: corrige uso das variaveis de ambiente para deploy no railway

--- src/helpers/auth.ts
+++ src/helpers/auth.ts
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken'
 
-const ACCESS_SECRET = process.env['ACCESS_SECRET'] ?? ''
-const ACCESS_EXPIRES = process.env['ACCESS_EXPIRES'] ?? ''
+const ACCESS_SECRET = process.env.ACCESS_SECRET ?? ''
+const ACCESS_EXPIRES = process.env.ACCESS_EXPIRES ?? ''
 
 export function generateTokens(dataToEncript: any) {
     return jwt.sign(dataToEncript, ACCESS_SECRET, { expiresIn: ACCESS_EXPIRES })
